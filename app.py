7a26ca68dd7b17d11a96a3c64e5bdfde76ec84a5
josesilveira865 <322509873+josesilveira865@users.noreply.github.com>
2026-09-13 18:52:11 -0300

    Fix syntax error in discount print statement

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ if compra < 200:
 elif compra < 300:
     print("Valor da compra: R$ %.2f - Desconto de 10%%" % compra)
 else:
-    print("Valor da compra: R$ %.2f - Desconto de 15%%" % compra
+    print("Valor da compra: R$ %.2f - Desconto de 15%%" % compra)
 
 
